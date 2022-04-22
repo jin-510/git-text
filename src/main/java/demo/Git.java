86d0a7a1347eb764ca7540b382536b367260d13c
jin <1207598510@qq.com>
2022-04-22 16:52:55 +0800

    push commit

--- src/main/java/demo/Git.java
+++ src/main/java/demo/Git.java
@@ -9,5 +9,6 @@ public class Git {
         System.out.println("master");
         System.out.println("hello,git4");
         System.out.println("hot-fix");
+        System.out.println("push");
     }
 }
